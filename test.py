a55e331df16d84b2db08d1b089b72484001a9d29
joomong0620 <yeon062000@naver.com>
2025-06-07 03:19:02 +0900

    Fix DB URL newline issue

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
+# test.py
+
+from dotenv import load_dotenv
 import os
-print(f"🔍 DATABASE_URL = '{os.getenv('DATABASE_URL')}'")
+
+load_dotenv()  # 👈 반드시 먼저 호출돼야 함
+
+print("✅ DATABASE_URL = ", repr(os.getenv("DATABASE_URL")))
